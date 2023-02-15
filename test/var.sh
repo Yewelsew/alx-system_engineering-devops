7848e6d6ea1ef82f2cf3747b8294d26f3880ea58
root <mess>
2023-02-15 01:05:16 -0800

    var

--- test/var.sh
+++ test/var.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-declare var='$mess'
-echo -n  "what is the value of mess..."
-cat $mess 
-echo "the varlue of mess is " 
-cat var='3*$mess' 
+declare -i var='$mess'
+echo -n  "what is the value of mess...";
+cat $mess; 
+echo "the varlue of mess is ";
+cat var='3*$mess';
